c1cc3488ae8f416f931895844a04a9f6a35ee7f1
Admin <vadimsuman55@gmail.com>
2025-11-30 21:37:54 +0700

    ClickDemo

--- app/src/main/java/com/example/gesturedemo/MainActivity.kt
+++ app/src/main/java/com/example/gesturedemo/MainActivity.kt
@@ -12,6 +12,28 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.gesturedemo.ui.theme.GestureDemoTheme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,9 +47,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-
+    ClickDemo(modifier)
 }
 
+@Composable
+fun ClickDemo(modifier: Modifier = Modifier) {
+    var colorState by remember { mutableStateOf(true)}
+    var bgColor by remember { mutableStateOf(Color.Blue) }
+    val clickHandler = {
+        colorState = !colorState
+        bgColor = if (colorState) {
+            Color.Blue
+        } else {
+            Color.DarkGray
+        }
+    }
+    Box(
+        modifier
+            .clickable { clickHandler() }
+            .background(bgColor)
+            .size(100.dp)
+    )
+}
 
 @Preview(showBackground = true)
 @Composable
